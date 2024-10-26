8eadde11c117e45bb5a23cb005eb2fdbd58b2e0d
NIGHT <3210103847@zju.edu.cn>
2024-10-26 13:51:11 +0800

    common data structure

---
+++ Basic/3.CommonDataStru.cpp
@@ -0,0 +1,93 @@
+# include <iostream>
+# include <stack>
+# include <queue>
+
+using namespace std;
+
+int main(){
+    // Array
+    int arr[5] = {1, 2, 3, 4, 5};
+    cout << arr[0] << endl; // output the first element
+
+    // Struct
+    struct Person{
+        string name;
+        int age;
+    };
+    Person p = {"Tom", 20};
+    cout << p.name << endl; // output the name of the person, and output a blank line
+
+    // Class
+    // 和java的class定义有点像
+    // class 可以理解为除了struct还有函数，即你可以利用这个struct里的数据作为哪些函数的输入，很方便的实现功能，一个class有一些特定的功能，比如dog就是可以bark 
+    class Student{
+        private:
+            string name;
+            int age;
+        
+        public:
+            // 构造函数
+            Student(string n , int a): name(n), age(a){};
+            // 定义成员函数
+            void printInfo(){
+                cout << "Name: "<<name<<", Age: "<<age << endl;
+            }
+    };
+    Student s("Bob", 18);
+    s.printInfo();
+
+    // 链表linked list（单向链表）
+    // 本质上也是一个struct
+    struct Node{
+        int data;
+        Node* next;
+    };
+    Node* head = nullptr; // Node*类型的head是一个空指针
+    Node* newNode = new Node{10, nullptr}; // 创建一个新的Node
+    head = newNode; // 将head指向这个新的Node
+
+    // 栈stack
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    cout << st.top() << endl; // output the top element
+    st.pop(); // pop the top element
+    cout << st.top() << endl; // output the top element (already changed)
+
+    // 队列queue
+    // 和栈stack类似，只是队列是先进先出，而栈是先进后出
+    queue<int> q;
+    q.push(11);
+    q.push(12);
+    cout << q.front() << endl; // output the front element
+    q.pop(); // pop the front element
+    cout << q.front() << endl; // output the front element (already changed)
+
+    // 双向队列deque
+    // deque是double-ended queue的缩写，即双向队列
+    // 可以在队列的两端进行插入和删除操作，是栈和队列的结合体
+    deque<int> dq;
+    dq.push_back(100);
+    dq.push_front(101);
+    dq.push_front(102);
+    cout << dq.front() << endl; // output the front element
+    cout << dq.back() << endl; // output the back element
+    dq.pop_front(); // pop the front element
+    cout << dq.front() << endl; // output the front element (already changed)
+
+    //哈希表hash table
+    // 哈希表是一种数据结构，它可以快速的插入和查找数据
+    
+
+
+ 
+
+
+    return 0;
+}
+
+
+
+
+
+
--- Basic/readme.md
+++ Basic/readme.md
@@ -2,6 +2,9 @@
 - 使用g++编译.cpp文件
 `g++  main.cpp -o main.out`
 
+- 默认生成a.exe
+`g++ main.cpp`
+
 ## 2. using namespace 
 
 ```cpp
@@ -10,4 +13,5 @@ using namespace std;
 这句代码表示外面使用standard library，所以就不用再写std
 
 
+https://www.runoob.com/cplusplus/cpp-data-structures.html#:~:text=C%2B%2B%20%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%20C%2B%2B%20%E6%8F%90%E4%BE%9B%E4%BA%86%E5%A4%9A%E7%A7%8D%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%EF%BC%8C%E6%97%A2%E6%9C%89%E5%9F%BA%E7%A1%80%E7%9A%84%E5%A6%82%E6%95%B0%E7%BB%84%E3%80%81%E7%BB%93%E6%9E%84%E4%BD%93%E3%80%81%E7%B1%BB%E7%AD%89%EF%BC%8C%E4%B9%9F%E6%9C%89%E9%AB%98%E7%BA%A7%E7%9A%84%20STL%20%E5%AE%B9%E5%99%A8%E5%A6%82,vector%20%E3%80%81%20map%20%E5%92%8C%20unordered_map%20%E7%AD%89%E3%80%82
 
