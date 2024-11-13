54418d2ce93cd3023f19c93906086f1675c85767
NIGHT <3210103847@zju.edu.cn>
2024-11-13 10:50:18 +0800

    Update main.rs

--- Rust/5hybridtype/src/main.rs
+++ Rust/5hybridtype/src/main.rs
@@ -1,4 +1,5 @@
 #![allow(unused_variables)] // 允许被定义变量未使用
+
 fn main() {
     // String
     cut();
@@ -9,6 +10,13 @@ fn main() {
 
     // tup元组
     tup();
+
+    // 结构体
+    structure();
+
+    // 枚举
+    enumeration();
+
 }
 
 // 切片
@@ -201,6 +209,7 @@ fn calculate_length(s: String) -> (String, usize) {
 // Structure结构体
 fn structure(){
     // 定义结构体
+    #[derive(Debug)] // 实现debug，为了能够使用println!打印结构体
     struct User {
         username: String,
         email: String,
@@ -216,6 +225,101 @@ fn structure(){
     };
     // 通过.访问结构体字段
     user1.email = String::from("anotheremail@example.com");
+    // 利用已有的结构体构建新的结构体（不用对复用的参数进行重新赋值）
+    // let user2 = User {
+    //     email: String::from("another@example.com"),
+    //     ..user1
+    // };
+
+    // 结构体赋值时也会发生所有权转义
+    // 但是只是部分字段的所有权转移
+    let user2 = User {
+        // username所有权被转移，而active作为bool类型，是Copy类型，不会发生所有权转移
+        active: user1.active,
+        username: user1.username, 
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count,
+    };
+    println!("{}", user1.active);
+    // 下面这行会报错
+    // println!("{:?}", user1);
+
+    // 元组结构体定义和赋值
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+    let _black = Color(0, 0, 0);
+    let _origin = Point(0, 0, 0);
+
+    // 单元结构体（空的结构体，只有特征）
+    struct AlwaysEqual;
+    let subject = AlwaysEqual;
+    // 定义一个特征，它有一个方法 `are_equal`，该方法返回一个布尔值。
+    trait SomeTrait {
+        fn are_equal(&self) -> bool;
+    }
+    // 我们不关心 AlwaysEqual 的字段数据，只关心它的行为，因此将它声明为单元结构体，然后再为它实现某个特征
+    impl SomeTrait for AlwaysEqual {
+        fn are_equal(&self) -> bool {
+            true
+        }
+    }
+
+    println!("{:?}", user2); // 开头加上#[derive(Debug)]，可以直接打印结构体
+
+    // 利用dbg宏输出debug信息
+    dbg!(&user2);
+    
+}
+// 利用函数构建结构体
+// fn build_user(email: String, username: String) -> User {
+//     User {
+//         email,
+//         username,
+//         active: true,
+//         sign_in_count: 1,
+//     }
+// }
+
+// 枚举
+// 枚举类型是一个类型，它会包含所有可能的枚举成员，而枚举值是该类型中的具体某个成员的实例。
+fn enumeration(){
+    // 定义枚举，以扑克牌花色为例
+    #[derive(Debug)]
+    enum PokerCard {
+        Clubs(u8),
+        Spades(u8),
+        Diamonds(char),
+        Hearts(char),
+    }
+      fn print_suit(card: PokerCard) {
+        // 需要在定义 enum PokerSuit 的上面添加上 #[derive(Debug)]，否则会报 card 没有实现 Debug
+        println!("{:?}",card);
+    }
+    // 枚举成员可以包含数据
+    // 利用::访问枚举成员
+    let heart = PokerCard::Hearts;
+    let diamond = PokerCard::Diamonds;
+    print_suit(heart);
+    print_suit(diamond);
+
+    // 枚举成员可以包含数据
+    // 同一枚举下的不同枚举成员可以包含不同类型的数据
+    let c1 = PokerCard::Spades(5);
+    let c2 = PokerCard::Diamonds('A');
+    println!("{:?}, {:?}", c1, c2);
+
+    // 枚举成员可以包含结构体，甚至另一个枚举
+    #[derive(Debug)]
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+    let m1 = Message::Quit;
+    let m2 = Message::Move{x:1,y:1};
+    let m3 = Message::ChangeColor(255,255,0);
+    println!("{:?}, {:?}, {:?}", m1, m2, m3);
 }
 
 
